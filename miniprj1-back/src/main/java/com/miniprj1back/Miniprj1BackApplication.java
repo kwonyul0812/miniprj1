1f942dd2e8d3c288d3a73bad1a389fd7029c45b6
kwonyul <kw990812@gmail.com>
2024-07-09 16:34:52 +0900

    first commit

---
+++ miniprj1-back/src/main/java/com/miniprj1back/Miniprj1BackApplication.java
@@ -0,0 +1,13 @@
+package com.miniprj1back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miniprj1BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miniprj1BackApplication.class, args);
+    }
+
+}
---
+++ miniprj1-back/src/test/java/com/miniprj1back/Miniprj1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniprj1back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miniprj1BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
